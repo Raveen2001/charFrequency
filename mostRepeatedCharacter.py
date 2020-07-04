9946fbf499ea908ec656edefff8f3a09d105a0a3
Raveen <raveenanbarasan@gmail.com>
2020-07-04 22:54:37 +0530

    final

---
+++ mostRepeatedCharacter.py
@@ -0,0 +1,17 @@
+from pprint import pprint
+sentence = "This is the text where we need to find which character is repeated the most!!"
+letters = {}
+most_frequency_char = []
+for char in sentence.lower():
+    letters[char] = letters.get(char, 0) + 1
+
+letters = sorted(letters.items(), key=lambda x: x[1], reverse=True)
+pprint(letters, width=12)
+most_frequency = letters[0][1]
+for letter, frequency in letters:
+    if frequency == most_frequency:
+        most_frequency_char.append(letter)
+    else:
+        break
+print("The most repeated character is ", most_frequency_char)
+sdfs
